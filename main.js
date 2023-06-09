d7eeb99f9a5fdcbaef5d6c0ff586ca1b420ef216
3DUTEX <132105117+3DUTEX@users.noreply.github.com>
2023-06-09 18:42:47 -0300

    carrossel responsivo

--- main.js
+++ main.js
@@ -5,3 +5,58 @@ hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+//Carrossel seta com looping - by: t3xdev
+let btnAnt = document.querySelector("#btnAnt");
+let btnProx = document.querySelector("#btnProx");
+let image = document.querySelector("#imageCarrossel");
+
+const timeInterval = 3000;
+let interval = setInterval(proxSlide, timeInterval);
+let count = 1;
+
+const oneImage = "Images/IMG.jpg";
+const twoImage = "Images/IMG1.jpg";
+const threeImage = "Images/IMG2.jpg";
+
+function slideAnt() {
+  clearInterval(interval);
+  interval = setInterval(proxSlide, timeInterval);
+  count--;
+  switch (count) {
+    case 1:
+      image.src = oneImage;
+      break;
+    case 2:
+      image.src = twoImage;
+      break;
+    case 3:
+      image.src = threeImage;
+      break;
+    default:
+      image.src = threeImage;
+      count = 3;
+      break;
+  }
+}
+
+function proxSlide() {
+  clearInterval(interval);
+  interval = setInterval(proxSlide, timeInterval);
+  count++;
+  switch (count) {
+    case 1:
+      image.src = oneImage;
+      break;
+    case 2:
+      image.src = twoImage;
+      break;
+    case 3:
+      image.src = threeImage;
+      break;
+    default:
+      image.src = oneImage;
+      count = 1;
+      break;
+  }
+}
